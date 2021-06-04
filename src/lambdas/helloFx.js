7471beeb93f79e379e2a2e94aa273ebdc298797f
harshit9715 <harshit9715@gmail.com>
2021-06-04 16:18:50 +0530

    upadted changes

--- src/lambdas/helloFx.js
+++ src/lambdas/helloFx.js
@@ -2,6 +2,8 @@
 exports.handler = async event => {
     return {
         statusCode: 200,
-        message: process.env.MESSAGE_STRING || 'Hello World!'
+        body: JSON.stringify({
+            message: process.env.MESSAGE_STRING || 'Hello World!'
+        })
     }
 }
